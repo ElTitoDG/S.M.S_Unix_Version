830a67a2aad9d9f2a9324c769240e35f95771921
Julián Hinojosa <julianhinojosa@MacBook-Air-de-Julian.local>
2022-07-17 00:10:30 +0200

    main app tui

--- src/main.c
+++ src/main.c
@@ -11,6 +11,14 @@ void printChar(char ch, int n);
 
 int main() {
 
+    // region: --- Main func variables
+
+    int option;
+
+    // endregion: --- Main func variables
+
+    // region: --- Main Title TUI
+
     title();
     printf("\n\n\t\t\t\t2022-2023 Academic Year");
     printf("\n\n\t\t\t\t Julián Hinojosa Gil");
@@ -19,5 +27,37 @@ int main() {
     printf("\n\n\n\t\t\t press any key to enter");
     getchar();
 
+    // endregion: --- Main Title TUI
+
+    while (1) {
+
+        title();
+        printf("\n\t");
+        printChar('*', 65);
+
+        printf("\n\n\t\t\t\t1. Add Student(WIP)");
+        printf("\n\n\t\t\t\t2. Modify Student(WIP)");
+        printf("\n\n\t\t\t\t3. Show all Students(WIP)");
+        printf("\n\n\t\t\t\t4. Individual View(WIP)");
+        printf("\n\n\t\t\t\t5. Remove Student(WIP)");
+        printf("\n\n\t\t\t\t6. Change Password(WIP)");
+        printf("\n\n\t\t\t\t7. Logout\n\n\t");
+        printChar('*', 65);
+
+        printf("\n\n\t\t\t\tEnter your option: ");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 7:
+                return 1;
+                break;
+            default:
+                printf("\n\t\tNo action selected");
+                printf("\n\t\tPress any key\n\n\n");
+                sleep(1);
+                getchar();
+        }
+
+    }
     return 1;
 }
