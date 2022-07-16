df163880461662226a1c34079fcc119ca91949b6
Julián Hinojosa <julianhinojosa@MacBook-Air-de-Julian.local>
2022-07-16 23:48:31 +0200

    main title tui

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <curses.h>
+#include <unistd.h>
 #include "../src/smslib.h"
 
 void title();
+void printChar(char ch, int n);
 
-int main(int argc, char *argv[]) {
+int main() {
 
     title();
+    printf("\n\n\t\t\t\t2022-2023 Academic Year");
+    printf("\n\n\t\t\t\t Julián Hinojosa Gil");
+    printf("\n\n\t\t\t\t San Agustin Institute\n\t\t\t");
+    printChar('=', 39);
+    printf("\n\n\n\t\t\t press any key to enter");
+    getchar();
 
-    return 0;
+    return 1;
 }
