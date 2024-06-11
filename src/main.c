ffde0a48020fa0c287029f5ee7f21c8cffbf55e4
ElTitoDG <julianhinojosagil@gmail.com>
2024-06-11 23:30:32 +0200

    New formating for main

--- src/main.c
+++ src/main.c
@@ -5,72 +5,75 @@
 
 #define sleep_time 1
 
+int main()
+{
+    // region: --- Main func variables
 
-int main() {
+    int option;
 
-  // region: --- Main func variables
+    // endregion: --- Main func variables
 
-  int option;
-
-  // endregion: --- Main func variables
-
-  // region: --- Main Title TUI
-
-  title();
-  printf("\n\n\t\t\t\t2022-2023 Academic Year");
-  printf("\n\n\t\t\t\t Julián Hinojosa Gil");
-  printf("\n\n\t\t\t\t San Agustin Institute\n\t\t\t");
-  printChar('=', 39);
-  printf("\n\n\n\t\t\t press any key to enter");
-  getchar();
-
-  // endregion: --- Main Title TUI
-
-  while (1) {
+    // region: --- Main Title TUI
 
     title();
-    mainmenu();
+    printf("\n\n\t\t\t\t2022-2023 Academic Year");
+    printf("\n\n\t\t\t\t Julián Hinojosa Gil");
+    printf("\n\n\t\t\t\t San Agustin Institute\n\t\t\t");
+    printChar('=', 39);
+    printf("\n\n\n\t\t\t press any key to enter");
+    getchar();
 
-    scanf("%d", &option);
+    // endregion: --- Main Title TUI
 
-    switch (option) {
-    case 1:
-      add();
-      sleep(sleep_time);
-      getchar();
-      break;
-    case 3:
-      printf("Function Here\n");
-      show();
-      sleep(sleep_time);
-      printf("\n\t\tPress any key to continue\n\n\t");
-      getchar();
-      break;
-    case 6:
-      password();
-      sleep(sleep_time);
-      printf("\n\t\tPress any key to continue\n\n\n");
-      getchar();
-      break;
-    case 7:
-      if (checkIfFileExists("test/a.txt")) {
-        printFileContent("test/a.txt");
-      } else {
-        printf("not exits");
-      }
-      sleep(sleep_time);
-      printf("\n\t\tPress any key to continue\n\n\n");
-      getchar();
-      break;
-    case 8:
-      return 1;
-      break;
-    default:
-      printf("\n\t\tNo action selected");
-      printf("\n\t\tPress any key to continue\n\n\n");
-      sleep(sleep_time);
-      getchar();
+    while (1)
+    {
+        title();
+        mainmenu();
+
+        scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+            add();
+            sleep(sleep_time);
+            getchar();
+            break;
+        case 3:
+            printf("Function Here\n");
+            show();
+            sleep(sleep_time);
+            printf("\n\t\tPress any key to continue\n\n\t");
+            getchar();
+            break;
+        case 6:
+            password();
+            sleep(sleep_time);
+            printf("\n\t\tPress any key to continue\n\n\n");
+            getchar();
+            break;
+        case 7:
+            if (checkIfFileExists("test/a.txt"))
+            {
+                printFileContent("test/a.txt");
+            }
+            else
+            {
+                printf("not exits");
+            }
+            sleep(sleep_time);
+            printf("\n\t\tPress any key to continue\n\n\n");
+            getchar();
+            break;
+        case 8:
+            return 1;
+            break;
+        default:
+            printf("\n\t\tNo action selected");
+            printf("\n\t\tPress any key to continue\n\n\n");
+            sleep(sleep_time);
+            getchar();
+        }
     }
-  }
-  return 1;
+    return 1;
 }
