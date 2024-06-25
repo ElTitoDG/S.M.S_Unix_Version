40e7e8904fcc8992f002ec329cdba40841803a87
ElTitoDG <julianhinojosagil@gmail.com>
2024-06-25 13:40:52 +0200

    added ESC key for window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
-#include "../include/smslib.h"
-#include "../include/misclib.h"
-#include "../deps/GLFW/glfw3.h"
+#include "include/smslib.h"
+#include "include/misclib.h"
+#include "deps/GLFW/glfw3.h"
+#define NK_IMPLEMENTATION
+#include "deps/nuklear.h"
 
 #define sleep_time 1
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(void)
 {
@@ -52,10 +64,17 @@ int main(void)
             case '8':
                 glfwInit();
 
-                GLFWwindow* window = glfwCreateWindow(1280, 720, "Todo", NULL, NULL);
-
+                GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+                glfwSetErrorCallback(error_callback);
+                if (!glfwInit())
+                    return EXIT_FAILURE;
+                if (!window)
+                {
+                    glfwTerminate();
+                    return EXIT_FAILURE;
+                }
                 glfwMakeContextCurrent(window);
-
+                glfwSetKeyCallback(window, key_callback);
                 while (!glfwWindowShouldClose(window))
                 {
                     glClear(GL_COLOR_BUFFER_BIT);
@@ -64,7 +83,6 @@ int main(void)
                     glfwPollEvents();
                     glfwSwapBuffers(window);
                 }
-
                 glfwDestroyWindow(window);
                 glfwTerminate();
                 break;
